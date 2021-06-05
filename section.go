d5980f9823524f1d256079c70b67cd06924aee7b
Seyed Javad Hosseini <javadh75@gmail.com>
2021-06-05 13:49:15 +0430

    Add section

---
+++ section.go
@@ -0,0 +1,12 @@
+package systemdconfig
+
+// Represents a setion and it's options.
+type Section struct {
+	Name    string
+	Options []*OptionValue
+}
+
+// NewSection returns a new section with pre-set name and empty options.
+func NewSection(name string) *Section {
+	return &Section{Name: name, Options: []*OptionValue{}}
+}
