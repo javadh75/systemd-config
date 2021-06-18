f8e6afefdebfb0a769b750f76efbaae30d278ab0
Seyed Javad Hosseini <javadh75@gmail.com>
2021-06-18 17:04:52 +0430

    Add section's match function

--- section.go
+++ section.go
@@ -10,3 +10,53 @@ type Section struct {
 func NewSection(name string) *Section {
 	return &Section{Name: name, Options: []*OptionValue{}}
 }
+
+func InitialCompareSliceGenerator(size int) []bool {
+	ICS := make([]bool, size)
+	for index, _ := range ICS {
+		ICS[index] = false
+	}
+	return ICS
+}
+
+func AllAreTrue(b []bool) bool {
+	for _, element := range b {
+		if !element {
+			return false
+		}
+	}
+	return true
+}
+
+func (s *Section) Match(other *Section) bool {
+	if s.Name != other.Name {
+		return false
+	}
+	if len(s.Options) != len(other.Options) {
+		return false
+	}
+
+	otherSeen := InitialCompareSliceGenerator(len(other.Options))
+
+	compareList := InitialCompareSliceGenerator(len(s.Options))
+
+	shouldBreak := false
+	for i, sElement := range s.Options {
+		for j, otherElement := range other.Options {
+			if sElement.Match(otherElement) {
+				compareList[i] = true
+				otherSeen[j] = true
+				break
+			} else if j == len(other.Options) {
+				shouldBreak = true
+			}
+		}
+		if shouldBreak {
+			break
+		}
+	}
+	if !AllAreTrue(otherSeen) || !AllAreTrue(compareList) {
+		return false
+	}
+	return true
+}
