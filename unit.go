8c250c876d561a1defbba536da107269e80a6e81
Seyed Javad Hosseini <javadh75@gmail.com>
2021-06-28 21:09:32 +0430

    feat: Add unit matching

--- unit.go
+++ unit.go
@@ -9,3 +9,29 @@ type Unit struct {
 func NewUnit() *Unit {
 	return &Unit{Sections: []*Section{}}
 }
+
+func (u *Unit) Match(other *Unit) bool {
+	if len(u.Sections) != len(other.Sections) {
+		return false
+	}
+
+	otherSeen := InitialCompareSliceGenerator(len(other.Sections))
+
+	compareList := InitialCompareSliceGenerator(len(u.Sections))
+
+	for i, uElement := range u.Sections {
+		for j, otherElement := range other.Sections {
+			if uElement.Match(otherElement) {
+				compareList[i] = true
+				otherSeen[j] = true
+				break
+			} else if j == len(other.Sections)-1 {
+				return false
+			}
+		}
+	}
+	if !AllAreTrue(otherSeen) || !AllAreTrue(compareList) {
+		return false
+	}
+	return true
+}
