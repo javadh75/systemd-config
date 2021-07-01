d1fe4f78bb960181264cfa68e85ca92eadd9515b
Seyed Javad Hosseini <javadh75@gmail.com>
2021-07-01 11:18:03 +0430

    Add toEOL() unit test

--- deserialize_test.go
+++ deserialize_test.go
@@ -1,7 +1,9 @@
 package systemdconfig
 
 import (
+	"bufio"
 	"io"
+	"reflect"
 	"strings"
 	"testing"
 )
@@ -84,6 +86,77 @@ func TestNewLexer(t *testing.T) {
 			}
 			if buf.String() != tt.args.s {
 				t.Errorf("NewLexer() got.buf = %v, want %v", buf.String(), tt.args.s)
+			}
+		})
+	}
+}
+
+func Test_lexer_toEOL(t *testing.T) {
+	type fields struct {
+		s string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    []byte
+		want1   bool
+		wantErr bool
+	}{
+		{
+			name: "EmptyLine",
+			fields: fields{
+				s: "\n",
+			},
+			want:    []byte{},
+			want1:   false,
+			wantErr: false,
+		},
+		{
+			name: "EmptyLineMS-DOS",
+			fields: fields{
+				s: "\r\n",
+			},
+			want:    []byte{},
+			want1:   false,
+			wantErr: false,
+		},
+		{
+			name: "SimpleLine",
+			fields: fields{
+				s: "SimpleLine\n",
+			},
+			want:    []byte("SimpleLine"),
+			want1:   false,
+			wantErr: false,
+		},
+		{
+			name: "EOF",
+			fields: fields{
+				s: "",
+			},
+			want:    []byte(""),
+			want1:   true,
+			wantErr: false,
+		},
+		// TODO Add test case to cover not EOF error
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			l := &lexer{
+				buf:     bufio.NewReader(strings.NewReader(tt.fields.s)),
+				lexchan: make(chan *lexData),
+				errchan: make(chan error, 1),
+			}
+			got, got1, err := l.toEOL()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("lexer.toEOL() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("lexer.toEOL() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("lexer.toEOL() got1 = %v, want %v", got1, tt.want1)
 			}
 		})
 	}
